a07778b5d1db142233ce74ea393a365bb705a863
andersonfsgit <afs10@cin.ufpe.br>
2018-04-16 09:56:58 -0300

    Implementando o item 10 do exercício

--- exercicio2-rss-master/RSS/app/src/main/java/br/ufpe/cin/if1001/rss/db/SQLiteRSSHelper.java
+++ exercicio2-rss-master/RSS/app/src/main/java/br/ufpe/cin/if1001/rss/db/SQLiteRSSHelper.java
@@ -73,10 +73,56 @@ public class SQLiteRSSHelper extends SQLiteOpenHelper {
     public long insertItem(ItemRSS item) {
         return insertItem(item.getTitle(),item.getPubDate(),item.getDescription(),item.getLink());
     }
+
+
     public long insertItem(String title, String pubDate, String description, String link) {
-        return 0.0;
+
+        // Obtendo a instância do banco de dados
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        // Criando um objeto, do tipo ContentValues, para facilitar a manipulação do banco
+        ContentValues cvalues = new ContentValues();
+
+        // Inserindo os parametros no objeto
+        cvalues.put(ITEM_TITLE, title);
+        cvalues.put(ITEM_DATE, pubDate);
+        cvalues.put(ITEM_DESC, description);
+        cvalues.put(ITEM_LINK, link);
+        cvalues.put(ITEM_UNREAD, false);
+
+        /*
+        // Verifica se já existe esse registro no banco
+        if (!isItemExits(db, link)) {
+            // Se o item não existir no banco, cria uma nova linha
+            db.insert(DATABASE_TABLE,null, cvalues);
+            db.close();
+        } else {
+            // Se o item já existir, atualiza a linha
+            db.update(DATABASE_TABLE, cvalues, link + " = ?",
+                    new String[] { String.valueOf(link)});
+            db.close();
+        }
+        */
+
+        return db.insert(DATABASE_TABLE,null, cvalues);
     }
+
+
+    // Obtendo um ItemRSS que está no banco de dados
+
     public ItemRSS getItemRSS(String link) throws SQLException {
+
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.query(DATABASE_TABLE, new String[] { ITEM_ROWID, ITEM_TITLE, ITEM_DATE, ITEM_DESC, ITEM_LINK, ITEM_UNREAD },
+                ITEM_LINK + "=?" , new String[] { String.valueOf(link)},
+                null,
+                null,
+                null,
+                null);
+
+        if (cursor "")
+
         return new ItemRSS("FALTA IMPLEMENTAR","FALTA IMPLEMENTAR","2018-04-09","FALTA IMPLEMENTAR");
     }
     public Cursor getItems() throws SQLException {
@@ -88,6 +134,21 @@ public class SQLiteRSSHelper extends SQLiteOpenHelper {
 
     public boolean markAsRead(String link) {
         return false;
+    }
+
+
+    // Verifica se o item já foi inserido no banco
+
+    public boolean isItemExits(SQLiteDatabase db, String rss_link) {
+
+        // Faz uma consulta ao banco e retorna 1 caracter por conta do SELECT 1, caso seja encontrado o item em questão, no banco
+        Cursor cursor = db.rawQuery("SELECT 1 FROM " + DATABASE_TABLE
+                + " WHERE rss_link = '" + rss_link + "'", new String[]{});
+
+        // Conta a quantidade o tamanho que o curso contém, diante da execução da query acima
+        boolean exists = (cursor.getCount() > 0);
+
+        return exists;
     }
 
 }
